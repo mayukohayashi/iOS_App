2dd5f08a7e9ea948d56f5a016ca2019188e29af7
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-04-02 19:43:22 +0900

    first commit

---
+++ I AM RICH/I AM RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Mayuko Hayashi on 2020/04/02.
+//  Copyright © 2020 Mayuko Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
