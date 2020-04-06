57ea7fc20db2cad5decde9d5329814376e2f8c26
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-04-06 15:02:13 +0900

    learn: familier with x-code

---
+++ 2nd_lesson/I AM POOR/I AM POOR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM POOR
+//
+//  Created by Mayuko Hayashi on 2020/04/06.
+//  Copyright © 2020 Mayuko Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
