16cdfff6e2468ae86ec884e7935782e093585a05
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-04-11 15:19:41 +0900

    learn: Button functions

--- 3rd_lesson_Dicee/Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ 3rd_lesson_Dicee/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
@@ -27,6 +28,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // just try how it works
+        // print("Button got pressed")
+        
+        // after pressed, dice number will change
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
 
+    }
+    
 }
 
