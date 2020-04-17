2135714cb42685627a7b0939304ac2d88088b55a
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-04-17 12:27:54 +0900

    refac: simplify codes

--- 3rd_lesson_Dicee/Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ 3rd_lesson_Dicee/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -18,26 +18,9 @@ class ViewController: UIViewController {
     var leftDiceNumber = 1
     var rightDiceNumber = 5
     
-        
-        // diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        // diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-        
-        // Who           What   Value
-        // diceImageView1.alpha = 0.5
-        
-        
-
-        // Do any additional setup after loading the view.
-    }
-
+    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        // just try how it works
-        // print("Button got pressed")
-        
-        // after pressed, dice number will change
-        // diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        // diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
-      
+  
         print("the biggining of dice number = \(leftDiceNumber)")
         
         diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ][leftDiceNumber]
@@ -49,10 +32,6 @@ class ViewController: UIViewController {
         
         print("the end of dice number = \(leftDiceNumber)")
         
-        
-
-        
-
     }
     
 }
