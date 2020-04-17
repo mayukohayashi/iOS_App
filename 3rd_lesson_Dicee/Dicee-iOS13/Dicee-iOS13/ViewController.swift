7a4ef1c667016e21d2d0c9ed415bd35a953cabe9
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-04-17 13:21:15 +0900

    learning: let, random

--- 3rd_lesson_Dicee/Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ 3rd_lesson_Dicee/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -14,23 +14,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    // show number is 2
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
-    
-    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ][Int.random(in: 0...5)]
-        diceImageView2.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ][Int.random(in: 0...5)]
+        // let == JS "const" like, constant, cannot change
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        
+        // [Int.random(in: 0...5)] == .randomElement()
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
 
-        
-        leftDiceNumber += 1
-        rightDiceNumber -= 1
-        
-        // random integer from 1 to 10
-        // Int.random(in: 1...10)
-        
     }
     
 }
